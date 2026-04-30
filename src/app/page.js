483fc08ddf425674701b28e9f43dab0fad785a9d
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-04-30 07:33:12 +0600

    create footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
-import Navbar from "@/components/share/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
-      <Navbar/>
     </div>
   );
 }
